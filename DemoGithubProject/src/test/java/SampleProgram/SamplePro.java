4edb061e3460529f3c87015ef2a85ca3ede43fc6
nagarajut7 <nagarajut7@gmail.com>
2022-07-02 21:52:43 +0530

    Added sample programs

---
+++ DemoGithubProject/src/test/java/SampleProgram/SamplePro.java
@@ -0,0 +1,17 @@
+package SampleProgram;
+
+public class SamplePro {
+
+	public static void main(String[] args) {
+
+		String name = "Nagaraju";
+		
+		if (name.equals("Nagaraju")) {
+			System.out.println("Name is correct");
+		}
+		else {
+			System.out.println("Incorrect name");
+		}
+	}
+
+}
